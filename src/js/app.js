242a35872e391c636c78e3388ffc6a81cf1aab5a
Wei <wei840220@gmail.com>
2021-01-08 19:44:43 +0800

    add cover when loaded

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,26 @@ import Animations from './animations.js';
 const sound = new Sound();
 const animations = new Animations();
 
+const sec = 3000;
+let timer = null;
+const resetTimer = () => {
+    if (timer)
+        clearTimeout(timer);
+    timer = setTimeout(() => toggleCover(true), sec);
+};
+const toggleCover = (state) => document.getElementById('cover').classList[state ? 'remove': 'add']('hidden');
+
 window.addEventListener('keydown', (e) => {
     if (e.keyCode > 64 && e.keyCode < 91) {
         const key = e.key.toLowerCase();
+        toggleCover(false);
+        resetTimer();
         sound.play({key});
         animations.play({key});
     }
+});
+
+window.addEventListener('load', function(e) {
+    toggleCover(true);
+    resetTimer();
 });
